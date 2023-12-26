abc32f588ed738f06ac568af23b4d184e541b6e6
furiousknight25 <honeybee25061@gmail.com>
2023-12-26 02:07:56 -0600

    Updated with a "working" player
    Everything is a bit unstable so be careful with touching stuff

--- Player.gd
+++ Player.gd
@@ -2,11 +2,55 @@ extends CharacterBody3D
 #im chosing the keep the player scene and script not in a folder because we will acess it a lot 
 #ima create a state machine for the player, simmilar system to the enimies, they will call the movement_c.gd to control movement based on their special state machine
 
+var fullscreen := false
+var speed := 12
+var rot_want 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var movementC = $MovementC
+var camera : Camera3D
+
+func _ready():
+	camera = get_tree().get_nodes_in_group("camera")[0]
 func _process(delta):
-	pass
+#region basic debug tool
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("fullscreen"):
+		if !fullscreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			fullscreen = true
+		else:
+			fullscreen = false
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	if Input.is_action_just_pressed('restart'):
+		get_tree().reload_current_scene()
+	
+#endregion
+	
+#region Input movement
+	movementC.rotation.y = camera.rotation.y
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var intent = (movementC.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	movementC.intent = intent
+	
+	if intent: # this is jank and will not work long term
+		movementC.cur_state = movementC.STATES.ACTIVE
+	if !intent:
+		movementC.cur_state = movementC.STATES.IDLE
+	
+	$velocity.position = position
+	$accel.position = position
+	$velocity.target_position = velocity
+	$accel.target_position = movementC.global_transform.basis.z * delta * movementC.base_acceleration * 50
+	
+#endregion
+	
+	#move_and_slide() #might have to find the difference between the movement_c.gd's velocity and this one, subtract them and add it to this velocity so that the characterbody3d move and slide can act accordenliy rather than setting. 
 
 
 func death():
 	queue_free()
+
+func _set_velocity(v, r):
+	velocity = v
+	rot_want = r
--- character_components/movement_c.gd
+++ character_components/movement_c.gd
@@ -1,13 +1,78 @@
 extends Node3D
 
 @export var body_to_move : CharacterBody3D
+@export var base_acceleration := 12
+@export var max_speed : float
+@export var friction := 12
+var camera : Camera3D
 
-@export var GRAVITY = 40
-# Called when the node enters the scene tree for the first time.
+enum STATES {IDLE, ACTIVE, GAP, DEPLOY}
+var cur_state = STATES.IDLE
+
+var velocity := Vector3.ZERO
+var rot : float
+var rot_speed := 10.0
+const GRAVITY = -100
+var intent : Vector3
+
+
+signal movement_data
+
 func _ready():
-	pass # Replace with function body.
+	camera = get_tree().get_nodes_in_group("camera")[0]
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	#print(velocity)
+	emit_signal('movement_data', velocity, rot)
+	body_to_move.move_and_slide()
+#region this dog is up bruh
+	match cur_state:
+		STATES.IDLE:
+			idle_process(delta)
+		STATES.ACTIVE:
+			active_process(delta)
+		STATES.GAP:
+			gap_process(delta)
+		STATES.DEPLOY:
+			deploy_process(delta)
+#endregion
+	
+	if !body_to_move.is_on_floor():# jank fix
+		velocity.y += GRAVITY * delta
+	else:
+		velocity.y = 0
+		velocity.x = move_toward(velocity.x, 0.0, 1 * delta)#naturally our body aligns itself, this is it.
+		velocity.z = move_toward(velocity.z, 0.0, 1 * delta)
+	
+#region state processes
+func idle_process(delta):
+	velocity.x = move_toward(velocity.x, 0.0, friction * delta)#naturally our body aligns itself, this is it.
+	velocity.z = move_toward(velocity.z, 0.0, friction * delta)
+	
+func active_process(delta):
+	if intent:
+		body_to_move.rotation.y = lerp_angle(body_to_move.rotation.y, atan2(intent.x, intent.z), delta * rot_speed)
+		velocity += body_to_move.global_transform.basis.z * delta * base_acceleration
+		velocity.x = clamp(velocity.x, -max_speed, max_speed)
+		velocity.z = clamp(velocity.z, -max_speed, max_speed)
+	
+func gap_process(delta):
 	pass
+	
+func deploy_process(delta):
+	pass
+	
+#endregion
+
+#region set state
+func set_state_idle():
+	cur_state = STATES.IDLE
+func set_state_active():
+	cur_state = STATES.ACTIVE
+func set_state_gap():
+	cur_state = STATES.GAP
+func set_state_deploy():
+	#prob do like play animation, and then have an animation trigger where it enables set state idle
+	cur_state = STATES.DEPLOY
+#endregion
